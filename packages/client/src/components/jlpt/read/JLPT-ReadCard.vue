08945d43609d04372752f543dd275a4e2b220768
YuzuTea <flin00806@gmail.com>
2025-02-24 18:41:25 +0800

    feat: 封装文本选中菜单

--- packages/client/src/components/jlpt/read/JLPT-ReadCard.vue
+++ packages/client/src/components/jlpt/read/JLPT-ReadCard.vue
@@ -25,64 +25,78 @@
                 </n-button>
             </div>
         </template>
+        
         <!-- 内容部分 -->
-        <div v-if="read.article?.contents" class="flex flex-col gap-5">
-            <div v-for="content in read.article.contents">
-                {{ content }}
-            </div>
-        </div>
-        <n-divider />
-        <!-- 词汇表 -->
-        <div class="flex flex-col gap-5" v-if="read.vocabList?.length">
-            <div class="text-lg">词汇表</div>
-            <div class="flex flex-wrap gap-5">
-                <JLPT_ReadVocabCard
-                    class="min-w-50 max-w-75 flex-1"
-                    v-for="vocab in read.vocabList"
-                    :vocab="vocab"
-                />
-            </div>
-        </div>
-        <n-divider />
-        <!-- 问题部分 -->
-        <div v-if="read.questions" class="flex flex-col gap-5">
-            <div v-for="(question, questionIndex) in read.questions" class="flex flex-col gap-5">
-                <!-- 题干 -->
-                <div class="flex items-center font-bold gap-2">
-                    <n-tag type="primary" size="small">{{ question.type }}</n-tag>
-                    <div>{{ question.question }}</div>
-                </div>
-                <!-- 选项 -->
-                <div
-                    class="ml-2.5 flex gap-2 items-center"
-                    v-for="(option, optionIndex) in question.options"
-                >
-                    <n-radio
-                        :checked="selects[questionIndex] === optionIndex + 1"
-                        :value="optionIndex + 1"
-                        :disabled="isSubmitted"
-                        @change="
-                            () => {
-                                selects[questionIndex] = optionIndex + 1
-                                answerCount++
-                            }
-                        "
-                    >
-                        {{ option }}
-                    </n-radio>
-                    <!-- 正确与错误 -->
-                    <template v-if="isSubmitted">
-                        <SuccessIcon v-if="optionIndex === question.answer" size="16" />
-                        <ErrorIcon v-else size="16" />
-                    </template>
-                </div>
-                <!-- 解析 -->
-                <div v-if="isSubmitted" class="text-gray ml-2.5 italic">
-                    {{ question.analysis }}
+        <TextSelectMenu>
+            <!-- 默认插槽内容 -->
+            <template #default>
+                <div v-if="read.article?.contents" class="flex flex-col gap-5">
+                    <div v-for="content in read.article.contents">
+                        {{ content }}
+                    </div>
                 </div>
                 <n-divider />
-            </div>
-        </div>
+                <!-- 词汇表 -->
+                <div class="flex flex-col gap-5" v-if="read.vocabList?.length">
+                    <div class="text-lg">词汇表</div>
+                    <div class="flex flex-wrap gap-5">
+                        <JLPT_ReadVocabCard
+                            class="min-w-50 max-w-75 flex-1"
+                            v-for="vocab in read.vocabList"
+                            :vocab="vocab"
+                        />
+                    </div>
+                </div>
+                <n-divider />
+                <!-- 问题部分 -->
+                <div v-if="read.questions" class="flex flex-col gap-5">
+                    <div
+                        v-for="(question, questionIndex) in read.questions"
+                        class="flex flex-col gap-5"
+                    >
+                        <!-- 题干 -->
+                        <div class="flex items-center font-bold gap-2">
+                            <n-tag type="primary" size="small">{{ question.type }}</n-tag>
+                            <div>{{ question.question }}</div>
+                        </div>
+                        <!-- 选项 -->
+                        <div
+                            class="ml-2.5 flex gap-2 items-center"
+                            v-for="(option, optionIndex) in question.options"
+                        >
+                            <n-radio
+                                :checked="selects[questionIndex] === optionIndex + 1"
+                                :value="optionIndex + 1"
+                                :disabled="isSubmitted"
+                                @change="
+                                    () => {
+                                        selects[questionIndex] = optionIndex + 1
+                                        answerCount++
+                                    }
+                                "
+                            >
+                                {{ option }}
+                            </n-radio>
+                            <!-- 正确与错误 -->
+                            <template v-if="isSubmitted">
+                                <SuccessIcon v-if="optionIndex === question.answer" size="16" />
+                                <ErrorIcon v-else size="16" />
+                            </template>
+                        </div>
+                        <!-- 解析 -->
+                        <div v-if="isSubmitted" class="text-gray ml-2.5 italic">
+                            {{ question.analysis }}
+                        </div>
+                        <n-divider />
+                    </div>
+                </div>
+            </template>
+
+            <!-- 自定义菜单
+            <template #menu="{ selectedText }">
+                <button @click="console.log(selectedText)">自定义操作</button>
+            </template> -->
+        </TextSelectMenu>
     </n-card>
 </template>
 
@@ -93,6 +107,7 @@ import { computed, ref } from 'vue'
 import JLPT_ReadVocabCard from './JLPT-ReadVocabCard.vue'
 import ErrorIcon from '@/components/icon/ErrorIcon.vue'
 import SuccessIcon from '@/components/icon/SuccessIcon.vue'
+import TextSelectMenu from '@/components/tools/TextSelectMenu.vue'
 
 const { read } = defineProps<{
     read: Partial<JLPT_Read>
